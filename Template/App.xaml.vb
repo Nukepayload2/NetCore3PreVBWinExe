c33ffe2e8a3f41d6e4c743df6b99c1608e46a7d9
Nukepayload2 <1939357182@qq.com>
2018-12-07 20:20:45 +0800

    Add template

---
+++ Template/App.xaml.vb
@@ -0,0 +1,3 @@
+﻿Partial Class App
+
+End Class
---
+++ Template/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Windows
+
+Partial Class MainWindow
+    Private Sub ButtonExit_Click(ByVal sender As Object, ByVal e As RoutedEventArgs) Handles ButtonExit.Click
+        Application.Current.Shutdown()
+    End Sub
+End Class

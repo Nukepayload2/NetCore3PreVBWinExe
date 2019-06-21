3215d5a834d9a5cb233463954277cbfe0e0d4f84
Nukepayload2 <1939357182@qq.com>
2019-06-21 23:51:22 +0800

    Added MsgBox to WPF template

--- WPF/MainWindow.xaml.vb
+++ WPF/MainWindow.xaml.vb
@@ -1,7 +1,9 @@
 ﻿Imports System.Windows
 
 Partial Class MainWindow
-    Private Sub ButtonExit_Click(ByVal sender As Object, ByVal e As RoutedEventArgs) Handles ButtonExit.Click
-        Application.Current.Shutdown()
+    Private Sub ButtonExit_Click(sender As Object, e As RoutedEventArgs) Handles ButtonExit.Click
+        If MsgBox("Are you sure you want to exit?", vbQuestion Or vbYesNo) = vbYes Then
+            Application.Current.Shutdown()
+        End If
     End Sub
 End Class
